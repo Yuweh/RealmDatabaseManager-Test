7e6ae149fc01cb5446419c85030d0db033e96f11
Francis Bergonia <francisb.itcareers@gmail.com>
2020-01-04 17:58:11 +0800

    Initial Commit

---
+++ RealmMedia-Test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmMedia-Test2
+//
+//  Created by Francis Jemuel Bergonia on 1/4/20.
+//  Copyright © 2020 Xi Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
